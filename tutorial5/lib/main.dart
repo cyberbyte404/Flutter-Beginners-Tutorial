786fb83bcbb7181b61e7c72a1b19c2c6a3ad6858
4purbo <joy.20151616518#gmail.com>
2023-06-12 17:35:15 +0300

    initialCommit

--- tutorial5/lib/main.dart
+++ tutorial5/lib/main.dart
@@ -1,56 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 157, 157, 157),
-      body: Center(
-        child: MyContainerExample(),
+void main() => runApp(
+      MaterialApp(
+        home: MyScreen(),
       ),
-    ),
-  ));
-}
+    );
 
-// class MyCardExample extends StatelessWidget {
-//   const MyCardExample({super.key});
+class MyScreen extends StatelessWidget {
+  MyScreen({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       color: Colors.transparent,
-//       elevation: 15,
-//       child: Text("I am floating right now !"),
-//     );
-//   }
-// }
+  // create/declare the variable & initialize at once
+  // variable_type variableName = variable_value;
+  int likedBy = 20;
 
-class MyContainerExample extends StatelessWidget {
-  const MyContainerExample({super.key});
+  String liker = 'generalcoding164';
+
+  // isLoading
+  bool isLoading = false;
+
+// soap, shampoo, eraser
+// => shampoo
+
+  List likers = [
+    "generalcoding164", // 0
+    "metalicGaming", // 1
+    "lucky's pleasure", // 2
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.transparent,
-      elevation: 15,
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.deepOrange,
-            border: Border.all(
-              color: Colors.green,
-              width: 5,
-            ),
-            borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(40),
-                bottomLeft: Radius.circular(40),
-                topLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10))),
-        width: 500,
-        height: 100,
-        child: const Center(
-          child: Text('I am inside the container right now'),
-        ),
+    return Scaffold(
+      body: Center(
+        child: isLoading
+            ? CircularProgressIndicator()
+            : Text("The value is: ${likers.length}"),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+// isLoading_DueToSomeReason
+
+// camelCase
+// => first word will be of small letters
+// => rest word's first letter will be capital
